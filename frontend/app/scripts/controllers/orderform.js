1e04c6d40aa4dbc77e8eb30c3b202cd1f5226e87
Cameron Lakenen <camupod@gmail.com>
2013-12-27 18:05:55 -0800

    such increment; many regex

--- frontend/app/scripts/controllers/orderform.js
+++ frontend/app/scripts/controllers/orderform.js
@@ -2,6 +2,7 @@
 
 angular.module('suchApp')
   .controller('OrderFormCtrl', function ($scope, $rootScope, $filter) {
+    var MAX_QUANTITY = 1e+8;
     var cryptoFilter = $filter('cryptoCurrency');
     var market = {
       market: 'DOGE',
@@ -30,4 +31,36 @@ angular.module('suchApp')
     $scope.market = market;
     $scope.order = order;
     $scope.txFee = $rootScope.txFee;
+
+    $scope.increment = function (event) {
+      var input = event.target,
+        start = input.selectionStart,
+        val = input.value,
+        pos = start === val.length ? start - 1 : start,
+        floatVal = parseFloat(val),
+        num = parseInt(val[pos], 10),
+        floatNum;
+
+      console.dir(event);
+      if (num > -1) {
+        var v = val.replace(/\d/g, '0');
+        v = v.substr(0, pos) + 1 + v.substr(pos + 1);
+        console.log(v, pos, num);
+        floatNum = parseFloat(v);
+
+        switch (event.keyCode) {
+          case 38: // UP
+            console.log(floatVal, floatNum);
+            input.value = cryptoFilter(Math.min(MAX_QUANTITY, floatVal + floatNum));
+            input.selectionStart = input.selectionEnd = start;
+            event.preventDefault();
+            break;
+          case 40: // DOWN
+            input.value = cryptoFilter(Math.max(0, floatVal - floatNum));
+            input.selectionStart = input.selectionEnd = start;
+            event.preventDefault();
+            break;
+        }
+      }
+    };
   });
--- frontend/app/scripts/filters/cryptocurrency.js
+++ frontend/app/scripts/filters/cryptocurrency.js
@@ -3,6 +3,6 @@
 angular.module('suchApp')
   .filter('cryptoCurrency', function () {
     return function (input) {
-      return (parseFloat(input) || 0).toFixed(8).replace(/^([0-9]*)\.00+$/, '$1');
+      return (parseFloat(input) || 0).toFixed(8)/*.replace(/^([0-9]*)\.00+$/, '$1')*/;
     };
   });
